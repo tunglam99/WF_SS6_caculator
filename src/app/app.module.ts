7c26f0f2b6ddd811b3fdcae38821381777a7ca20
Nguyen Tung Lam <lamnthnue.edu.vn@gmail.com>
2019-11-05 00:58:34 +0700

    'ok'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CaculatorComponent } from './caculator/caculator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CaculatorComponent
   ],
   imports: [
     BrowserModule
